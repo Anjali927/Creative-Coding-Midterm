58f22f19350c139d22353f1ee6c821f2c66704b5
Anjali927 <axs9613@rit.edu>
2022-10-30 19:25:14 -0400

    Separate file for Ghost class

---
+++ Ghost.js
@@ -0,0 +1,64 @@
+class Ghost {
+    constructor() {
+      this.tail = [];
+      this.tailLength = 30;
+  
+      // Give ghost a random size and starting location
+      this.ghostSize = random(10, 100);
+      this.ghostX = random(width);
+      this.ghostY = random(height);
+  
+      this.cosOffset = random(100);
+      this.wiggliness = random(2, 10);
+      this.floatiness = random(2, 10);
+  
+  
+      // Give ghost a random color
+      this.r = random(255);
+      this.g = random(255);
+      this.b = random(255);
+    }
+  
+    moveAndDraw() {
+  
+      // Move ghost left and right in canvas
+      this.ghostX += cos((this.cosOffset + frameCount) / 10) * this.wiggliness;
+      
+      // Move ghost up canvas
+      this.ghostY -= this.floatiness;
+      
+      // Start ghost at the bottom again once it reaches top of canvas
+      if (this.ghostY < -this.ghostSize) {
+        this.ghostY = height + this.ghostSize;
+      }
+  
+      this.tail.unshift({x: this.ghostX, y: this.ghostY});
+      
+      // Remove last point if array is too large
+      if (this.tail.length > this.tailLength) {
+        this.tail.pop();
+      }
+  
+      // loop over tail and draw points
+      for (let index = 0; index < this.tail.length; index++) {
+        const tailPoint = this.tail[index];
+        // make tail smaller and more transparent
+        const pointSize = this.ghostSize * (this.tail.length - index) / this.tail.length;
+        const pointAlpha = 255 * (this.tail.length - index) / this.tail.length;
+        fill(this.r, this.g, this.b, pointAlpha);
+        ellipse(tailPoint.x, tailPoint.y, pointSize);
+      }
+  
+      // draw ghost's face
+      fill(32);
+      ellipse(this.ghostX - this.ghostSize * .2,
+              this.ghostY - this.ghostSize * .1,
+              this.ghostSize * .2);
+      ellipse(this.ghostX + this.ghostSize * .2,
+              this.ghostY - this.ghostSize * .1,
+              this.ghostSize * .2);
+      ellipse(this.ghostX,
+              this.ghostY + this.ghostSize * .2,
+              this.ghostSize * .2);
+    }
+  }
