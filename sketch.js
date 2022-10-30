d7fae28583dc30473b4e42add451f56db41abcaf
Anjali927 <axs9613@rit.edu>
2022-10-30 17:54:26 -0400

    update ghost object

--- sketch.js
+++ sketch.js
@@ -1,30 +1,104 @@
-//Midterm Progress - bendy line object
+//Midterm Progress - bendy line object and ghost object
 //Anjali Shiyamsaran
+//October 31
+
 let _minWidth;
 let t = 0.0;
-
-let y = 0;
-let x = 0;
+let lineArr = [];
 
 function setup() {
   createCanvas(600, 600);
   setSquiggly();
-  
-  
+  ghost = new GhostObject();
 }
-
-let lineArr = [];
 
 //set up stroke and ensure that it is responsive to canvas size
 function setSquiggly() {
   _minWidth = min(width, height) * 0.8;
-  strokeWeight(_minWidth/40);
-  stroke(0, 255, 0);
-  noFill();
-  strokeJoin(ROUND);
   let numberLine = 1;
   for (let i = 0; i < numberLine; i++) {
     lineArr[i] = new BendSquiggly();
+  }
+}
+
+class GhostObject {
+  constructor() {
+    this.ran = random(2);
+    this.ghostSize = 5;
+	this.osci = 10 * sin(t);
+	this.oscic = 10 * cos(t);
+	this.hW = 200 / this.ghostSize;
+	this.hH = 200 / (this.ghostSize - 1) + (this.ran == 1 ? this.osci / 5 : this.oscic / 5);
+  }
+  
+  display() {
+    noStroke();
+    translate(mouseX, mouseY);
+    push();
+    
+    //leg shadow
+    fill(200);
+    beginShape();
+    vertex(-this.hW, 0);
+    vertex(this.hW, 0);
+    for (let i = this.hW; this.i > -this.hW + 1; i -= 1) {
+      this.y = this.hH + (this.hH / 10) * cos(i / (this.hW / 500) + t);
+      vertex(i, this.y);
+      }
+    vertex(-this.hW, this.hH);
+    endShape();
+
+    //ghost body
+    this.bodycol = ["#ffffff", "#fbfefb"];
+    fill(this.bodycol[int(random(this.bodycol.length))]);
+
+    beginShape();
+    vertex(this.hW, 0);
+    bezierVertex(this.hW * 1.1, -this.hH * 1.35, -this.hW * 1.1, -this.hH * 1.35, -this.hW, 0);
+    vertex(-this.hW, this.hH);
+    for (let i = -this.hW; this.i < this.hW + 1; this.i += 1) {
+      this.y = this.hH + (this.hH / 10) * sin(i / (this.hW / 500) - t);
+      vertex(this.i, this.y);
+    }
+    vertex(this.hW, 0);
+    endShape();
+    pop();
+
+    //eye
+    this.mouthcol = color("#b38184");
+    this.eyecol = color("#574f7d");
+    if (this.ran == 1) {
+      fill(this.eyecol);
+      strokeCap(ROUND);
+      ellipse(-this.hW / 2, -hH / 2 + abs(this.osci / 5), this.hW / 5);
+      ellipse(this.hW / 5, -hH / 2 + abs(this.osci / 5), this.hW / 5);
+      push();
+      stroke(this.mouthcol);
+      fill(this.mouthcol);
+      strokeWeight(this.hW / 10);
+      ellipse(
+        -this.hW / 6.5,
+        -this.hH / 4 + abs(this.osci / 5),
+        this.hW / 6 + 2 * sin(this.t / 2),
+        (this.hW / 6) * sin(this.t / 2)
+      );
+      pop();
+      } else {
+        stroke(this.eyecol);
+        strokeWeight(this.hW / 10);
+        noFill();
+        arc(this.hW / 2, -this.hH / 2 + abs(this.oscic / 5), this.hW / 5, this.hW / 5, 360, 180);
+        arc(-this.hW / 5, -this.hH / 2 + abs(this.oscic / 5), this.hW / 5, this.hW / 5, 360, 180);
+        push();
+        fill(this.mouthcol);
+        stroke(this.mouthcol);
+        ellipse(
+          this.hW / 5.5,
+          -this.hH / 4 + abs(this.oscic / 5),
+          this.hW / 6 + 2 * abs(this.oscic / 5) * 0.3,
+          (this.hW / 6) * abs(this.oscic / 5) * 0.3
+        );
+    }
   }
 }
 
@@ -114,90 +188,18 @@ function bend(aryXy, rad, initAng, angStep, numberCycle) {
 //call update and draw methods
 function draw() {
   background(0);
-  translate(width/2, height/2); //keep in center of canvas
+  stroke(0, 255, 0);
+  strokeWeight(_minWidth/40);
+  noFill();
+  strokeJoin(ROUND);
+  translate(300, 300); //keep in center of canvas
   for (let i = 0; i < lineArr.length; i++) {
     lineArr[i].update();
     lineArr[i].draw();
   }
-  
+  ghost.display();
 }
 
 function mousePressed() {
-  translate(width/2, height/2);
-  let ran = random(2);
-  let ghostSize = 5;
-	let osci = 10 * sin(t);
-	let oscic = 10 * cos(t);
-	let hW = 200 / ghostSize;
-	let hH = 200 / (ghostSize - 1) + (ran == 1 ? osci / 5 : oscic / 5);
-	noStroke();
-	push();
-	translate(x, y);
-	push();
-	//leg_shadow-----
-	fill(200);
-	beginShape();
-	vertex(-hW, 0);
-	vertex(hW, 0);
-	for (let i = hW; i > -hW + 1; i -= 1) {
-		let y = hH + (hH / 10) * cos(i / (hW / 500) + t);
-		vertex(i, y);
-	}
-	vertex(-hW, hH);
-	endShape();
-
-	//ghost_body-----
-	let bodycol = ["#ffffff", "#fbfefb"];
-	fill(bodycol[int(random(bodycol.length))]);
-
-	beginShape();
-	vertex(hW, 0);
-	bezierVertex(hW * 1.1, -hH * 1.35, -hW * 1.1, -hH * 1.35, -hW, 0);
-	vertex(-hW, hH);
-	for (let i = -hW; i < hW + 1; i += 1) {
-		let y = hH + (hH / 10) * sin(i / (hW / 500) - t);
-		vertex(i, y);
-	}
-	vertex(hW, 0);
-	endShape();
-	pop();
-
-	//eye-------
-
-	let mouthcol = color("#b38184");
-	let eyecol = color("#574f7d");
-	if (ran == 1) {
-		fill(eyecol);
-		// strokeCap(ROUND);
-		ellipse(-hW / 2, -hH / 2 + abs(osci / 5), hW / 5);
-		ellipse(hW / 5, -hH / 2 + abs(osci / 5), hW / 5);
-		push();
-		stroke(mouthcol);
-		fill(mouthcol);
-		strokeWeight(hW / 10);
-		ellipse(
-			-hW / 6.5,
-			-hH / 4 + abs(osci / 5),
-			hW / 6 + 2 * sin(t / 2),
-			(hW / 6) * sin(t / 2)
-		);
-		pop();
-	} else {
-		stroke(eyecol);
-		strokeWeight(hW / 10);
-		noFill();
-		arc(hW / 2, -hH / 2 + abs(oscic / 5), hW / 5, hW / 5, 360, 180);
-		arc(-hW / 5, -hH / 2 + abs(oscic / 5), hW / 5, hW / 5, 360, 180);
-		push();
-		fill(mouthcol);
-		stroke(mouthcol);
-		ellipse(
-			hW / 5.5,
-			-hH / 4 + abs(oscic / 5),
-			hW / 6 + 2 * abs(oscic / 5) * 0.3,
-			(hW / 6) * abs(oscic / 5) * 0.3
-		);
-		pop();
-	}
-	pop();
+  //ghost.display();
 }
