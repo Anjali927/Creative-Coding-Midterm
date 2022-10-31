f7a59e50ea6f3c96b0e319d3ea208e5298e3605b
Anjali927 <axs9613@rit.edu>
2022-10-30 20:12:52 -0400

    Update Ghost location

--- Ghost.js
+++ Ghost.js
@@ -8,7 +8,7 @@ class Ghost {
       //give ghost a random size and starting location
       this.ghostSize = random(10, 100);
       this.ghostX = random(0);
-      this.ghostY = random(height);
+      this.ghostY = random(mouseY);
   
       this.cosOffset = random(100);
       this.wiggle = random(2, 10);
